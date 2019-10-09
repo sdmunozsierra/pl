6ac8ac5be9db573a4c8b6787b3bda2508bf966d0
sdmunozsierra <sdmunozsierra@miners.utep.edu>
2019-10-09 21:48:40 +0000

    Added arguments to ./decode.sh

--- bash/decode.sh
+++ bash/decode.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
-awk -F: 'BEGIN {FS=" "} {if(length($1) == 4) print $2}' fishlist.txt |
+fishlist=$1;  # First argument
+codebook=$2;  # Second argument
+
+awk -F: 'BEGIN {FS=" "} {if(length($1) == 4) print $2}' $fishlist |
 sort -n |
 awk -F '-' '{if($0%2) printf ("%c\n", $2+3)}' |
-awk 'FNR==NR { a[$1] = $2; next } $1 in a {print a[$1]}' codebook.txt - |
+awk 'FNR==NR { a[$1] = $2; next } $1 in a {print a[$1]}' $codebook - |
 tr '\n' ' '  > solution.txt
 cat solution.txt
